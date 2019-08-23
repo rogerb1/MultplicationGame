38678915f06b28c450e11652f4990f035d1b63fb
rogerdavid2 <rogerb7@uw.edu>
2019-08-22 20:23:44 -0700

    add commented code on commented.js

--- commented.js
+++ commented.js
@@ -1,5 +1,17 @@
 "use strict";
 
+/*
+    This game was created using composition software design 
+    principles. 
+ 
+    This is the commented program.
+
+    ROGER: 08/22/19
+
+*/
+
+// Multiplies two numbers
+// and returns the answer
 const equation = (state) => ({
     multiply() {
         let equation = state.number * state.numberTwo;
@@ -8,6 +20,8 @@ const equation = (state) => ({
     }
 });
 
+// Takes two parameters and 
+// returns the equation function.
 function numbers(number, numberTwo) {
     let state = {
         number,
@@ -21,7 +35,11 @@ function numbers(number, numberTwo) {
 
 let test = numbers(Math.floor(Math.random() * 12), Math.floor(Math.random() * 12));
 const answer = test.multiply(0, 0);
+console.log("Answer: " + answer);
 
+// Creates an array with four indicies
+// takes that state as a parameter
+// Displays all arrays on Dom
 const wholeArray = (state) => ({
     createArray() {
         let numberArray = [];
@@ -42,10 +60,19 @@ function array() {
     )
 }
 
+// The earlier returned value as become the state of the
+// function. I called whole array because thats the function
 let storeValue = wholeArray(answer);
+
+// This is where the array is stored
 let producedArray = storeValue.createArray();
+
 let newArray = [...producedArray];
 
+console.log("New array: " + newArray);
+
+// Shuffles array with the Fisher-Yates algorithm
+// Displays array on the DOM
 const shuffleArray = (state) => ({
     shuffledArray() {
         for (let i = newArray.length - 1; i > 0; i--) {
@@ -58,6 +85,7 @@ const shuffleArray = (state) => ({
         const one = document.getElementById('one').textContent = (newArray[1]);
         const two = document.getElementById('two').textContent = (newArray[2]);
         const three = document.getElementById('three').textContent = (newArray[3]);
+        console.log("After shuffle: " + newArray);
     }
 });
 
@@ -74,6 +102,10 @@ function sArray(newArray) {
 let hey = sArray(newArray);
 hey.shuffledArray();
 
+
+//  Function that starts the whole game
+
+// Loop over all buttons
 let cards = Array.from(document.getElementsByClassName('button'));
 cards.forEach(card => {
     card.addEventListener('click', () => {
